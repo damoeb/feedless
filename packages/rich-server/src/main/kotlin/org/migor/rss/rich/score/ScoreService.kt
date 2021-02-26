ae7a5225b4199b832ba3ce51471c4f77e8eff310
Markus Ruepp <mag@panter.ch>
2021-02-26 19:47:57 +0100

    remove stanford-corenlp

--- packages/rich-server/src/main/kotlin/org/migor/rss/rich/score/ScoreService.kt
+++ packages/rich-server/src/main/kotlin/org/migor/rss/rich/score/ScoreService.kt
@@ -1,43 +1,12 @@
 package org.migor.rss.rich.score
 
-import edu.stanford.nlp.ling.CoreAnnotations
-import edu.stanford.nlp.pipeline.StanfordCoreNLP
-import edu.stanford.nlp.sentiment.SentimentCoreAnnotations
 import org.migor.rss.rich.model.SourceEntry
 import org.springframework.stereotype.Service
-import java.util.*
-import javax.annotation.PostConstruct
 
 @Service
 class ScoreService {
 
-  private lateinit var pipeline: StanfordCoreNLP
-
-  @PostConstruct
-  fun onInit() {
-    val props = Properties()
-    props.setProperty("annotators", "tokenize, ssplit, pos, lemma, parse, sentiment")
-    pipeline = StanfordCoreNLP(props)
+  fun score(sourceEntry: SourceEntry) {
   }
 
-  fun score(sourceEntry: SourceEntry): Triple<Double, Double, Double> {
-    val text = listOfNotNull(sourceEntry.title!!, sourceEntry.content)
-      .joinToString(separator = "\n")
-    val annotation = pipeline.process(text)
-    val sentences = annotation.get(CoreAnnotations.SentencesAnnotation().javaClass)
-
-    val groups = sentences
-      .map { sentence -> sentence.get(SentimentCoreAnnotations.SentimentClass().javaClass) }
-      .groupBy { s: String -> s }
-
-    return Triple(fraction(groups["Positive"], sentences), fraction(groups["Neutral"], sentences), fraction(groups["Negative"], sentences))
-  }
-
-  private fun fraction(sentencesInGroup: List<String>?, all: List<Any>): Double {
-    return if (sentencesInGroup == null) {
-      0.0
-    } else {
-      sentencesInGroup.size / all.size.toDouble()
-    }
-  }
 }
