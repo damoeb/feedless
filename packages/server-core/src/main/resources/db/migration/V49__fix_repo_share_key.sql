ed37e618f0259ec80801f02e99e7141f802e5ba0
Markus Ruepp <mag@panter.ch>
2024-08-29 13:35:14 +0200

    fix migration V49__fix_repo_share_key.sql

--- packages/server-core/src/main/resources/db/migration/V49__fix_repo_share_key.sql
+++ packages/server-core/src/main/resources/db/migration/V49__fix_repo_share_key.sql
@@ -1,1 +1,1 @@
-UPDATE t_repository SET share_key = substring(gen_random_uuid()::varchar, 0, 9) where t_repository.visibility = 'isPrivate' and share_key=='';
+UPDATE t_repository SET share_key = substring(gen_random_uuid()::varchar, 0, 9) where t_repository.visibility = 'isPrivate' and share_key='';
