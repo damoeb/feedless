060d8d074b8f117ce62277f38384ca4458e78950
Markus Ruepp <mag@panter.ch>
2024-08-29 12:46:46 +0200

    gradle build with shared cache

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,9 +9,16 @@ if [ "$LATEST_COMMIT" = "$LAST_COMMIT" ]; then
 else
 
   # -Dorg.gradle.caching.debug=true
-  docker run -u "$UID:$GID" --workdir /opt/feedless -v "${PWD}:/opt/feedless" -it amazoncorretto:21 cd /opt/feedless && ./gradlew --no-daemon prepare
+  docker run -u "$UID:$GID" \
+    --workdir /opt/feedless \
+    -v "${PWD}:/opt/feedless" \
+    -v "${PWD}/gradle-build-cache:/opt/feedless/build-cache" \
+    -it amazoncorretto:21 cd /opt/feedless && ./gradlew --no-daemon prepare
   #docker run -u "$UID:$GID" --workdir /opt/feedless/packages/app-web -v "${PWD}:/opt/feedless" -it zenika/alpine-chrome:117-with-node npm run test:ci
-  docker run -u "$UID:$GID" -v "${PWD}:/opt/feedless" -it amazoncorretto:21 cd /opt/feedless && ./gradlew --no-daemon build -x packages:app-web:test
+  docker run -u "$UID:$GID" \
+    -v "${PWD}:/opt/feedless" \
+    -v "${PWD}/gradle-build-cache:/opt/feedless/build-cache" \
+    -it amazoncorretto:21 cd /opt/feedless && ./gradlew --no-daemon build -x packages:app-web:test
   ./gradlew --no-daemon bundle -x packages:app-web:test
 
 #  docker-compose stop feedless-app feedless-agent feedless-core
