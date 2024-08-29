a4de1d0d7bc52234bbcd733474b92895320502db
Markus Ruepp <mag@panter.ch>
2024-08-29 12:49:52 +0200

    gradle build with shared cache fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,12 +12,12 @@ else
   docker run -u "$UID:$GID" \
     --workdir /opt/feedless \
     -v "${PWD}:/opt/feedless" \
-    -v "${PWD}/gradle-build-cache:/opt/feedless/build-cache" \
+    -v "${PWD}/build-cache:/opt/feedless/build-cache" \
     -it amazoncorretto:21 cd /opt/feedless && ./gradlew --no-daemon prepare
   #docker run -u "$UID:$GID" --workdir /opt/feedless/packages/app-web -v "${PWD}:/opt/feedless" -it zenika/alpine-chrome:117-with-node npm run test:ci
   docker run -u "$UID:$GID" \
     -v "${PWD}:/opt/feedless" \
-    -v "${PWD}/gradle-build-cache:/opt/feedless/build-cache" \
+    -v "${PWD}/build-cache:/opt/feedless/build-cache" \
     -it amazoncorretto:21 cd /opt/feedless && ./gradlew --no-daemon build -x packages:app-web:test
   ./gradlew --no-daemon bundle -x packages:app-web:test
 
