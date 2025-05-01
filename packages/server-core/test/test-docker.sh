abaf6b68a2b57259f42a1ac626d46e3d8355c52d
damoeb <damoeb@feedless.org>
2025-05-01 22:17:57 +0200

    make dns test docker independent

--- packages/server-core/test/test-docker.sh
+++ packages/server-core/test/test-docker.sh
@@ -1,5 +1,15 @@
 # test DNS
 GIT_HASH=$1
 echo "Using gitHash $GIT_HASH"
-podman run damoeb/feedless:core-${GIT_HASH} nslookup br.de | grep br.de
+# Use DOCKER_BIN if set, otherwise fallback to podman
+CONTAINER_BIN="${DOCKER_BIN:-podman}"
+
+# Run the container and check DNS resolution for br.de
+if "$CONTAINER_BIN" run --rm damoeb/feedless:core-"${GIT_HASH}" nslookup br.de | grep -q br.de; then
+    echo "DNS resolution succeeded"
+else
+    echo "DNS resolution failed"
+    exit 1
+fi
+
 #docker run damoeb/feedless:core-arm nslookup br.de | grep br.de
