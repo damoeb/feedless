7fc253645a1a6b51672d784d1076402f3cee5e5a
damoeb <damoeb@feedless.org>
2025-05-01 22:36:45 +0200

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,6 @@
 echo "Starting deployment"
-echo "> Stopping services"
-docker-compose stop feedless-ingress feedless-app feedless-agent feedless-core && \
-echo "> Removing services" && \
-docker-compose rm -f feedless-ingress feedless-app feedless-agent feedless-core && \
-echo "> Starting services" && \
-docker-compose up --detach feedless-ingress feedless-app feedless-agent feedless-core && \
-docker-compose scale feedless-agent=2
-echo "Done"
+docker run -d -p 5000:5000 registr
+docker push localhost:5000/damoeb/feedless:app-latest
+docker push localhost:5000/damoeb/feedless:core-latest
+docker push localhost:5000/damoeb/feedless:agent-latest
+kubectl apply -f k8s
