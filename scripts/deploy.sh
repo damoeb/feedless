abb57ec1086fb1902325e67db5d62155a7c0f56f
damoeb <damoeb@feedless.org>
2025-05-01 22:51:59 +0200

    add cleanup to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,17 @@
 echo "Starting deployment"
-docker run -d -p 5000:5000 registr
+registry_container_id=$(docker run -d -p 5000:5000 registry)
+docker tag damoeb/feedless:app-latest localhost:5000/damoeb/feedless:app-latest
+docker tag damoeb/feedless:core-latest localhost:5000/damoeb/feedless:core-latest
+docker tag damoeb/feedless:agent-latest localhost:5000/damoeb/feedless:agent-latest
 docker push localhost:5000/damoeb/feedless:app-latest
 docker push localhost:5000/damoeb/feedless:core-latest
 docker push localhost:5000/damoeb/feedless:agent-latest
 kubectl apply -f k8s
+
+
+# wait 5min
+sleep 300
+
+# Clean up the registry container at the end
+docker stop "$registry_container_id"
+docker rm "$registry_container_id"
