98978a2850df0e436ce3c5d4090893697b4f0729
damoeb <damoeb@feedless.org>
2025-05-28 22:01:18 +0200

    deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 echo "Starting deployment"
-registry_container_id=$(docker run -d -p 5000:5000 registry)
+#registry_container_id=$(docker run -d -p 5000:5000 registry)
 docker tag damoeb/feedless:app-latest localhost:5000/damoeb/feedless:app-latest
 docker tag damoeb/feedless:core-latest localhost:5000/damoeb/feedless:core-latest
 docker tag damoeb/feedless:agent-latest localhost:5000/damoeb/feedless:agent-latest
@@ -7,11 +7,3 @@ docker push localhost:5000/damoeb/feedless:app-latest
 docker push localhost:5000/damoeb/feedless:core-latest
 docker push localhost:5000/damoeb/feedless:agent-latest
 kubectl apply -f k8s/feedless-agent.yaml k8s/feedless-web.yaml k8s/feedless-core.yaml k8s/grafana.yaml k8s/prometheus.yaml k8s/loki.yaml k8s/plausible.yaml k8s/postgis.yaml
-
-
-# wait 5min
-sleep 300
-
-# Clean up the registry container at the end
-docker stop "$registry_container_id"
-docker rm "$registry_container_id"
