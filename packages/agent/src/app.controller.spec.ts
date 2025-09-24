d16358615230f2278b9511a16510733edf57ee63
damoeb <damoeb@feedless.org>
2025-09-24 21:14:18 +0200

    fix(agent) tests

--- packages/agent/src/app.controller.spec.ts
+++ packages/agent/src/app.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SocketSubscriptionModule } from './services/socket-subscription/socket-subscription.module';
+import { CommonModule } from './services/common/common.module';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -9,6 +11,7 @@ describe('AppController', () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
+      imports: [SocketSubscriptionModule, CommonModule],
     }).compile();
 
     appController = app.get<AppController>(AppController);
