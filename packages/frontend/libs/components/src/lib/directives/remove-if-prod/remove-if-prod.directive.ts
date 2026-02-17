f5c9b1226dad685b8b96c5e0761a51fdda6679d6
damoeb <damoeb@feedless.org>
2026-02-17 23:18:20 +0100

    fix(frontend) SSR compatibility of remove-if-prod.directive

--- packages/frontend/libs/components/src/lib/directives/remove-if-prod/remove-if-prod.directive.ts
+++ packages/frontend/libs/components/src/lib/directives/remove-if-prod/remove-if-prod.directive.ts
@@ -1,18 +1,37 @@
-import { Directive, ElementRef, inject, OnInit, Renderer2 } from '@angular/core';
-import { environment } from '@feedless/core';
+import {
+  Directive,
+  ElementRef,
+  inject,
+  isDevMode,
+  OnInit,
+  PLATFORM_ID,
+  Renderer2,
+} from '@angular/core';
+import { isPlatformServer } from '@angular/common';
 
 @Directive({ selector: '[appDev]', standalone: true })
 export class RemoveIfProdDirective implements OnInit {
   private el = inject(ElementRef);
   private renderer = inject(Renderer2);
+  private platformId = inject(PLATFORM_ID);
 
   ngOnInit(): void {
-    if (environment.production) {
+    const isServer = isPlatformServer(this.platformId);
+    const isProduction = !isDevMode();
+
+    if (isServer || isProduction) {
       // If it's production, remove the element from the DOM
-      this.renderer.removeChild(this.el.nativeElement.parentNode, this.el.nativeElement);
+      this.renderer.removeChild(
+        this.el.nativeElement.parentNode,
+        this.el.nativeElement,
+      );
     } else {
       // If it's not production, add a red border to the element
-      this.renderer.setStyle(this.el.nativeElement, 'border', '2px solid magenta');
+      this.renderer.setStyle(
+        this.el.nativeElement,
+        'border',
+        '2px solid magenta',
+      );
     }
   }
 }
