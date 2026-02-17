89ab8ff463b369abe4efb2608040b6df1df96fe3
damoeb <damoeb@feedless.org>
2026-02-17 23:17:47 +0100

    fix(deployment) add upcoming service

--- scripts/deploy-feedless.sh
+++ scripts/deploy-feedless.sh
@@ -1,9 +1,11 @@
 echo "Starting deployment"
 #registry_container_id=$(docker run -d -p 5000:5000 registry)
 docker tag damoeb/feedless:app-latest localhost:5000/damoeb/feedless:app-latest
+docker tag damoeb/feedless:app-upcoming-latest localhost:5000/damoeb/feedless:app-upcoming-latest
 docker tag damoeb/feedless:core-latest localhost:5000/damoeb/feedless:core-latest
 docker tag damoeb/feedless:agent-latest localhost:5000/damoeb/feedless:agent-latest
 docker push localhost:5000/damoeb/feedless:app-latest
+docker push localhost:5000/damoeb/feedless:app-upcoming-latest
 docker push localhost:5000/damoeb/feedless:core-latest
 docker push localhost:5000/damoeb/feedless:agent-latest
 
@@ -12,6 +14,9 @@ kubectl rollout restart deployment feedless-agent
 
 kubectl apply -f k8s/feedless/feedless-web.yaml
 kubectl rollout restart deployment feedless-web
+
+kubectl apply -f k8s/feedless/upcoming-service.yaml
+kubectl rollout restart deployment upcoming-service
 
 kubectl apply -f k8s/feedless/feedless-core.yaml
 kubectl rollout restart deployment feedless-core
